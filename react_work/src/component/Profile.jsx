ddaaf8449fd103ee7b650eb8f9b2b0e90782bd17
ITC-2023B0131138 <rohitkumar142017@gmail.com>
2025-10-31 12:51:26 +0530

    31

--- react_work/src/component/Profile.jsx
+++ react_work/src/component/Profile.jsx
@@ -5,6 +5,7 @@ export default function Profile({ name, branch, section, college }) {
   return (
     <div className='container'>
       <h3>This is my profile page</h3>
+      <img src="img1.webp" alt="Profile Picture" height={100} width={150}/>
       <h2>Name: {name}</h2>
       <h2>Branch: {branch}</h2>
       <h2>Section: {section}</h2>
